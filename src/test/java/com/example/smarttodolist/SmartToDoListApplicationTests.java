37718c409ad9e5e777008262bf83f6035590cc9b
Savva <sav.van.ivan3@mail.ru>
2025-11-26 15:18:48 +0300

    Initial commit

---
+++ src/test/java/com/example/smarttodolist/SmartToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smarttodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
